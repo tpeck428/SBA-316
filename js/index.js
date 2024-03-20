4d65e244fb2418fbfc17f2716cfec0055a176954
tpeck428 <toripeck73@gmail.com>
2024-03-19 22:06:27 -0400

    added click event listener, commented out code

--- js/index.js
+++ js/index.js
@@ -2,5 +2,23 @@
 // let h1Tag = document.querySelector('h1');
 // console.log(h1Tag);
 
-//Adding 'required' to input in forms
-let formEl = document.querySelector('forms');
+//Added 'required' to input in forms
+//testing code
+// let formEl = document.getElementById('forms');
+// // console.log(formEl);
+
+// formEl.addEventListener('submit', (evt) =>{
+//     evt.preventDefault();
+//     // alert(`You submitted:
+//     // ${formEl.querySelector('input'.value)}`);
+//     return 'nothing submitted'
+// })
+
+//Event Listener for Button
+let btn = document.querySelector('button');
+console.log(btn);
+
+btn.addEventListener('click', function (evt){
+    console.log(evt);
+
+})
