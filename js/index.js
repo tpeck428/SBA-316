3d236a19bfc319d72214518802a93f3b31630c35
tpeck428 <toripeck73@gmail.com>
2024-03-20 23:11:01 -0400

    added working create element and append child methods; other changes

--- js/index.js
+++ js/index.js
@@ -1,24 +1,29 @@
-
-// let h1Tag = document.querySelector('h1');
-// console.log(h1Tag);
-
-//Added 'required' to input in forms
-//testing code
-// let formEl = document.getElementById('forms');
-// // console.log(formEl);
-
-// formEl.addEventListener('submit', (evt) =>{
-//     evt.preventDefault();
-//     // alert(`You submitted:
-//     // ${formEl.querySelector('input'.value)}`);
-//     return 'nothing submitted'
-// })
-
-//Event Listener for Button
-let btn = document.querySelector('button');
+//Cache at least one element with query.Selector
+let btn = document.querySelector('button'); //calling by id use # calling by class use .
 console.log(btn);
 
-btn.addEventListener('click', function (evt){
-    console.log(evt);
+//Event Listener for Button
+btn.addEventListener('click', handleClick);
+function handleClick(evt) {
+    alert(evt.target);
+}
 
-})
+//Centering the Header using .style
+let h1 = document.querySelector('h1');
+console.log(h1);
+h1.style.textAlign = 'center';
+
+//create element, append child
+const h3 = document.createElement("h3");
+console.log(h3);
+h3.innerText = 'Venture into a new world of dice!'
+document.querySelector("#form1").appendChild(h3);
+
+
+//non working code
+// btn.addEventListener('register', (evt) =>{
+//         evt.preventDefault();
+//         // alert(`You submitted:
+//         // ${btn.querySelector(input.value)}`);
+//         // return 'nothing submitted'
+//     })
